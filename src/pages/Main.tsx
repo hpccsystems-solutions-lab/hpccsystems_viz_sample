028be235425fc60eaab226a08d8ac9c356e19122
arjunachala <arjunachala@gmail.com>
2021-10-21 12:26:19 -0400

    CSS and other updates

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,8 @@
 import {
   Col,
   Layout,
-  Row
+  Row,
+  Table
  } from "antd";
 
 import {QueryHPCCData} from "../utils/QueryHPCCData";
@@ -18,7 +19,14 @@ export function Main() {
     title: {text: 'Population Vaccinated'},
     data: [],
     xField: 'date',
-    yField: 'daily_vaccinations'
+    yField: 'daily_vaccinations',
+    yAxis: {
+      title: {text: 'Vaccinations'}
+    },
+    xAxis: {
+      title: {text: 'Date'}
+    }
+
   }
 
   useEffect(() => {
@@ -29,17 +37,35 @@ export function Main() {
     }) 
   }, [])
 
+  function computeTableColumns(data: any) {
+    let columns: any[] = [];
+    if (data.length > 0) {
+      //Use the first row to construct the columns
+      Object.keys(data[0]).forEach((key) => {
+        if ("_row_id_" !== key)
+          //ignore the decorated column used to produce a unique row key
+          columns.push({ title: key, dataIndex: key, key: key });
+      });
+    }
+    return columns;
+  }
+
   return (
     <Layout style={{paddingTop: 10, paddingLeft:30, paddingRight:30}}>
-        <Row>
+        <Row style={{ paddingBottom: 10, paddingTop: 5}}>
           <Col  span={24} >
+            <div className={"label"}>Daily Vaccinations</div>
             <Chart chart={Column} config={vaccineChart} data={vaccineData} />
           </Col>
         </Row>
 
-        <Row>
+        <Row style={{ paddingBottom: 10, paddingTop: 5}}>
           <Col span={24}>
              {/* Data Table */}
+             <div className={"label"}>Vaccinations Data</div>
+             <div style={{overflow: "auto", height: 400}}>
+             <Table columns={computeTableColumns(vaccineData)}  rowKey={"date"} dataSource={vaccineData}/>
+             </div>
           </Col>
         </Row>
     </Layout>
